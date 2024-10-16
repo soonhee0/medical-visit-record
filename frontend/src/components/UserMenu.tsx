e7d2387159cb276890bad0649dcd04a724c5fb13
soonhee0 <kyoujunki0@gmail.com>
2024-10-16 13:19:20 +0900

    UserMenuページを追加

--- frontend/src/components/UserMenu.tsx
+++ frontend/src/components/UserMenu.tsx
@@ -1,50 +1,51 @@
 // MenuButtonコンポーネントを使ってボタンのスタイルと機能を設定する
-"use client"; 
-import React from 'react';
-import MenuButton from './MenuButton';
+"use client";
+import React, { ReactElement } from "react";
+import MenuButton from "./MenuButton";
+// react-iconsのFaUserアイコンをインポート
+import { FaUserCircle } from "react-icons/fa";
 
-interface Button{
-   
-     label:string; //ボタンに表示するラベル
-    onClick:()=>void;//ボタンがクリックされたときの処理
+interface Button {
+  label: string; //ボタンに表示するラベル
+  onClick: () => void; //ボタンがクリックされたときの処理
+  icon: ReactElement;
 }
 
-
-interface UserMenuProps{
-    userName:string;
-    buttons:Button[];
+interface UserMenuProps {
+  userName: string;
+  buttons: Button[];
 }
 
+const UserMenu: React.FC<UserMenuProps> = ({ userName, buttons }) => {
+  return (
+    <div className="bg-white-100 p-4 rounded-lg shadow-lg">
+      {/* ヘッダー部分 */}
+      <div className="bg-blue-100 w-full h-12 flex items-center px-4 rounded-t-lg mb-4">
+        {/* アイコンを追加 */}
+        <FaUserCircle className="text-gray-800 mr-2 align-middle " />
 
-
-const UserMenu:React.FC<UserMenuProps>=({userName,buttons})=>{
-   
-    return (
-     
-
-    //   divタグ内を下記と置き換える
-        <div className="bg-blue-100 p-4 rounded-lg shadow-lg">
-            <div className="flex items-center mb-4">
-       
-                 <span className="text-xl font-semibold text-gray-800"> {userName}さんのメニュー</span>
-            </div>
-            <div className="space-y-2">
-              {/* 各ボタンに異なるテキストとクリックイベントを渡す */}
-            {/* buttonsは各ボタンの情報（ラベルやクリックイベント）を格納している配列 */}
-                {buttons.map((button,index)=>(
-                    // 各ボタンの情報を利用してメニューボタンを生成する
-                    <MenuButton
-                        key={index}
-                        // ボタンクリック時の処理を指定
-                        onClick={button.onClick}
-                        className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
-                    
-                    >
-                        {button.label}
-                    </MenuButton> 
-                ))} 
-            </div> 
-        </div>
-    )
-}
-export default  UserMenu
+        <span className="text-xl font-semibold text-gray-800">
+          {userName}さんのメニュー
+        </span>
+      </div>
+      {/* ボディ部分 */}
+      <div className="flex flex-col space-y-2">
+        {/* 各ボタンに異なるテキストとクリックイベントを渡す */}
+        {/* buttonsは各ボタンの情報（ラベルやクリックイベント）を格納している配列 */}
+        {buttons.map((button, index) => (
+          // 各ボタンの情報を利用してメニューボタンを生成する
+          <MenuButton
+            key={index}
+            // ボタンクリック時の処理を指定
+            onClick={button.onClick}
+            className="bg-white text-gray-800 flex items-center space-x-2 px-4 py-2 rounded-lg border hover:bg-blue-600"
+          >
+            <span className="mr-2">{button.icon}</span>
+            {button.label}
+          </MenuButton>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default UserMenu;

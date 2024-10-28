6671201fd2d785b607eee60d7ca47cd1dd61668e
soonhee0 <kyoujunki0@gmail.com>
2024-10-28 10:31:03 +0900

    feat/GETメソッドを追加

--- frontend/src/app/api/users/[id]/route.ts
+++ frontend/src/app/api/users/[id]/route.ts
@@ -3,18 +3,19 @@ import { dummyData } from "../../dummyData/dummy";
 
 export async function DELETE(
   _: NextRequest,
+  // { params }は分割代入。関数に渡されたオブジェクトからidというプロパティを取り出し型を指定している
   { params }: { params: { id: string } }
 ) {
   const id = params.id;
 
   // mockでデータを削除
   dummyData.splice(
+    // .findIndex():dummyData 配列内の各要素を調べ、指定されたテスト関数を満たす最初の要素のインデックスを返す
     dummyData.findIndex((user) => user.id === parseInt(id)),
     1
   );
 
-  // ここでデータベースからユーザーを削除する処理を行います
-  // この例ではダミーの処理を行います
+
   console.log(`Deleting user with id: ${id}`);
 
   // 実際のアプリケーションでは、ここでデータベース操作を行います
@@ -24,3 +25,22 @@ export async function DELETE(
     { status: 200 }
   );
 }
+
+export async function GET(
+  _: NextRequest,
+  { params }: { params: { id: string } }
+){
+  const id = params.id;
+  const user=dummyData.find((user) => user.id === parseInt(id));
+  if(!user){
+    return NextResponse.json(
+      { message: `User with  ${id} doesn't exist` },
+      { status: 404 }
+    );
+  }
+  return NextResponse.json(
+    { message: `User with  ID ${id} found successfully` },
+    { status: 200 }
+  );
+}
+

7eb072fa95d4dc69a29b13be0c55221c20e1d1a0
soonhee0 <kyoujunki0@gmail.com>
2024-10-30 14:41:19 +0900

    add/フォームの値をAPI経由で送受信する

--- frontend/src/app/users/create-record/page.tsx
+++ frontend/src/app/users/create-record/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import React, { useState } from "react";
+import { ApiUrl } from "./../../constants/url";
 
 const MedicalRecordFormPage: React.FC = () => {
+  // フォームに入力した内容をsetformDataで保存している
   const [formData, setformData] = useState({
     visitDate: "",
     medicalInstitution: "",
@@ -17,15 +19,15 @@ const MedicalRecordFormPage: React.FC = () => {
     useState("");
   const [examinationContentErrorMessage, setExaminationContentErrorMessage] =
     useState("");
-  // const [nextVisitDateErrorMessage,setNextVisitDateErrorMessage]=useState("");
-  // 送信処理
-  const onSubmit = (e: React.FormEvent) => {
+
+  const onSubmit = async (e: React.FormEvent) => {
+    // フォームが送信される際にページがリロードされるデフォルトの動作を防いでいる
     e.preventDefault();
     // エラーメッセージの初期化
     setVisitDateErrorMessage("");
     setMedicalInstitutionErrorMessage("");
     setExaminationContentErrorMessage("");
-    // setNextVisitDateErrorMessage("");
+
     // エラーのチェック
     if (!formData.visitDate) {
       setVisitDateErrorMessage("通院日を入力してください");
@@ -36,21 +38,43 @@ const MedicalRecordFormPage: React.FC = () => {
     if (!formData.examinationContent) {
       setExaminationContentErrorMessage("診察メモを入力してください");
     }
-    // const emptyVisitDate=setVisitDateErrorMessage("通院日を入力してください");
-    // const emptyMedicalInstitutionErrorMessage=setMedicalInstitutionErrorMessage("医療機関名を入力してください")
-    // const emptyExaminationContent=setExaminationContentErrorMessage("診察メモを入力してください")
 
     // エラーメッセージがない場合に送信可能
     const enableSubmit =
       !visitDateErrorMessage &&
       !medicalInstitutionErrorMessage &&
       !examinationContentErrorMessage;
+    // クリックしてエラーに引っ掛からなければ、fetchで/api/users/create-recordへformDataを送っている
     if (enableSubmit) {
-      // フォームデータを送信する処理をここに追加
-      console.log("フォームデータを送信します", formData);
+      try {
+        const response = await fetch(
+          `${ApiUrl.BASE_API_URL}/api/users/create-record`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData), // formDataをJSONに変換
+          }
+        );
+        // レスポンスの処理
+        if (response.ok) {
+          const returnBody = await response.json();
+          console.log("成功:", returnBody);
+          // データ送信成功時にアラートを表示
+          alert("データ送信が成功しました！");
+        } else {
+          const errorResponse = await response.json();
+          console.error("エラー:", errorResponse);
+        }
+      } catch (error) {
+        console.error("送信中にエラーが発生しました:", error);
+      }
     }
   };
+
   return (
+    // フォームが送信されるたびにonSubmit関数が呼ばれる
     <form onSubmit={onSubmit}>
       <input type="text" name="name" />
       <button type="submit">送信</button>
